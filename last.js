c4eeed59338745de7111369b402fafa5110be411
Richard Griffiths <soulfiremage@hotmail.co.uk>
2019-07-10 11:02:36 +0100

    Update last.js
    I'm just fiddling with learning some Javascript and thought I'd contribute a fat arrow version using the more recent syntax :).

--- last.js
+++ last.js
@@ -2,3 +2,6 @@ function last(list){
 	var last = arguments[arguments.length - 1];
 	return last[last.length - 1] || last;
 }
+
+//Alternate syntax = later version of Javascript
+//let last = list => list.slice(list.length-1);
